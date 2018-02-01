df26bed96a67988c704f35b65a151b8a069af7ca
KEINOS <github@keinos.com>
2018-02-01 21:06:07 +0900

    style: prettier indenting and order

--- src/index.php
+++ src/index.php
@@ -1,14 +1,16 @@
 <?php
 
 // Replacement values. See 'box.json' at parent dir
-$git_version   = '@git_versionz@';
-$git_commit_id = '@git_commit_id@';
-$git_tag_id    = '@git_tag_id@';
-$date_build    = '@date_build@';
+$git_version      = '@git_version@';
+$git_commit_long  = '@git_commit_long@';
+$git_commit_short = '@git_commit_short@';
+$git_tag          = '@git_tag@';
+$date_build       = '@date_build@';
 
 echo 'Hello world!' . PHP_EOL;
 
-echo 'Git version   : ' . $git_version   . PHP_EOL;
-echo 'Git commit ID : ' . $git_commit_id . PHP_EOL;
-echo 'Git tag ID    : ' . $git_tag_id    . PHP_EOL;
-echo 'Date build    : ' . $date_build    . PHP_EOL;
+echo 'Git version       : ' . $git_version      . PHP_EOL;
+echo 'Git commit(long)  : ' . $git_commit_long  . PHP_EOL;
+echo 'Git commit(short) : ' . $git_commit_short . PHP_EOL;
+echo 'Git tag           : ' . $git_tag          . PHP_EOL;
+echo 'Date build        : ' . $date_build       . PHP_EOL;
